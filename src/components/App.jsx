7fb4cda1e652705193a78892457c8a834f9f63c0
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-11-06 00:14:59 +0200

    Update (useContext)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,13 @@
-import {useState} from "react";
+import {useContext} from "react";
 import Container from "./Container/Container";
 import Section from "./Section/Section";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Statistics from "./Statistics/Statistics";
 import Notification from "./Notification/Notification";
+import { FeedbackContext } from "./FeedbackContext/FeedbackContext";
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  const options = {
-    good: setGood,
-    neutral: setNeutral,
-    bad: setBad,
-  }
-
-  const onLeaveFeedback = ({ target: { name }}) => {
-    options[name](prev => prev + 1);
-  }
-
-  const countTotalFeedback = () => {
-    return good + neutral + bad;
-  }
-
-  const countPositiveFeedbackPercentage = () => {
-    const totalFeedback = good + neutral + bad;
-    if (good === 0) {
-      return 0;
-    }
-    return Math.round((good / totalFeedback) * 100);
-  }
+  const { good, neutral, bad, options, onLeaveFeedback, countTotalFeedback, countPositiveFeedbackPercentage } = useContext(FeedbackContext);
 
   return (
     <>
---
+++ src/components/FeedbackContext/FeedbackContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+
+export const FeedbackContext = createContext();
+
+export const FeedbackContextProvider = ({children}) => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = {
+    good: setGood,
+    neutral: setNeutral,
+    bad: setBad,
+  }
+
+  const onLeaveFeedback = ({ target: { name }}) => {
+    options[name](prev => prev + 1);
+  }
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  }
+
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedback = good + neutral + bad;
+    if (good === 0) {
+      return 0;
+    }
+    return Math.round((good / totalFeedback) * 100);
+  }
+
+  return (
+    <FeedbackContext.Provider value={{ good, neutral, bad, options, onLeaveFeedback, countTotalFeedback, countPositiveFeedbackPercentage } }>
+      {children}
+    </FeedbackContext.Provider>
+  );
+}
